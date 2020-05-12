70ffac451a36f5ac938154ffe3d66f7f2731c910
Lukas Rasmussen <lukasrasmusseen@gmail.com>
2020-05-12 13:42:47 +0200

    Angular skeleton structure WIP

--- angular-experiment/src/app/app.module.ts
+++ angular-experiment/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TenantViewComponent } from './tenant-view/tenant-view.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import { RemoveWorkGroupComponent } from './remove-work-group/remove-work-group.component';
+import { AddUserToGroupComponent } from './add-user-to-group/add-user-to-group.component';
+import { RemoveUserFromGroupComponent } from './remove-user-from-group/remove-user-from-group.component';
+import { RemoveUserComponent } from './remove-user/remove-user.component';
+import { AddWorkGroupComponent } from './add-work-group/add-work-group.component';
+import { ListTenantsComponent } from './list-tenants/list-tenants.component';
 
 @NgModule({
   declarations: [
-    AppComponent, TenantViewComponent, AddUserComponent
+    AppComponent, TenantViewComponent, AddUserComponent, RemoveWorkGroupComponent,
+    AddUserToGroupComponent, RemoveUserFromGroupComponent, RemoveUserComponent, AddWorkGroupComponent, ListTenantsComponent
   ],
   imports: [
     BrowserModule,
